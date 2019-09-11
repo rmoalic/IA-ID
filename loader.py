016cf22c499e2c7667967dbfb9f5da155e79e28b
MaximeF <fumelmax@gmail.com>
2019-09-11 16:40:24 +0200

    fix: exception handling + some PEP and doc

--- loader.py
+++ loader.py
@@ -4,11 +4,11 @@ import shelve
 
 FT = "fourre-tout"
 
+
 def load_flows(file_name):
+    """Load a list of flows from an xml file"""
     flows = list()
-
     root = ET.parse(file_name).getroot()
-
     for flow in root:
         flow_dict = dict()
         for e in flow:
@@ -16,19 +16,24 @@ def load_flows(file_name):
         flows.append(flow_dict)
     return flows
 
-def load_files(dirA):
-    files = [path.join(dirA,x) for x in listdir(dirA) if ".xml" in x]
-    with shelve.open(FT) as ft:
+
+def load_files(dir_a):
+    """Load flows from all xml files in a directory"""
+    files = [path.join(dir_a, x) for x in listdir(dir_a) if ".xml" in x]
+    with shelve.open(FT) as f_t:
         for f in files:
             print("loading file {}".format(f))
             try:
                 flows = load_flows(f)
-                ft[f] = flows
-            except e:
+                f_t[f] = flows
+            except MemoryError:
+                print('Not enough RAM.')
+            except Exception as e:
                 print(e)
+
 
 if not path.isfile(FT):
     load_files('./ISCX_train')
 
 with shelve.open(FT) as ft:
-   print(list(ft.keys()))
+    print(list(ft.keys()))
