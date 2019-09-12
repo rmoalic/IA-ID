ad34cdd483d31a1b22b2aad26691322d8937ed1e
polms <robin.moalic@yahoo.fr>
2019-09-12 14:34:13 +0200

    progress

--- intruvu/__main__.py
+++ intruvu/__main__.py
@@ -10,6 +10,12 @@ with shelve.open(FT, 'c') as ft:
 ft = shelve.open(FT, 'r')
 
 flow = Flow(ft[list(ft.keys())[0]])
+print(flow.get_protocols_count())
 print(flow.get_protocols())
-
+print(flow.get_flows_for_protocol("igmp"))
+print(flow.get_applications_count())
+print(flow.get_applications())
+print(flow.get_flows_for_applications("WebMediaAudio"))
+print(flow.get_protocols_bytes("tcp_ip"))
+print(flow.get_protocols_packets("tcp_ip"))
 ft.close()
--- intruvu/flow.py
+++ intruvu/flow.py
@@ -1,3 +1,10 @@
+from collections import Counter
+from itertools import groupby
+from functools import lru_cache
+
+#TODO get the source and destination Payload size for each protocol
+#     get the source and destination Payload size for each application
+
 
 class Flow:
 
@@ -5,10 +12,49 @@ class Flow:
         self.flow = flow
 
     def get_protocols(self):
-        t = [flow.get("protocolName", None) for flow in self.flow]
-        return set(t)
+        protocols_count = self.__count("protocolName")
+        return list(protocols_count.keys())
+
+    def get_protocols_count(self):
+        protocols_count = self.__count("protocolName")
+        return dict(protocols_count)
+
+    def get_protocols_packets(self, protocol):
+        protocols_packets = self.__aggregate_in_out("totalSourcePackets","totalDestinationPackets","protocolName")
+        return protocols_packets.get(protocol, None)
+
+    def get_protocols_bytes(self, protocol):
+        protocols_bytes = self.__aggregate_in_out("totalSourceBytes","totalDestinationBytes","protocolName")
+        return protocols_bytes.get(protocol, None)
 
     def get_flows_for_protocol(self, protocol):
+        return self.__get_flows("protocolName", protocol)
+
+    def get_applications(self):
+        applications_count = self.__count("appName")
+        return list(applications_count.keys())
+
+    def get_applications_count(self):
+        applications_count = self.__count("appName")
+        return dict(applications_count)
+
+    def get_flows_for_applications(self, app):
+        return self.__get_flows("appName", app)
+
+    @lru_cache(maxsize=5)
+    def __aggregate_in_out(self, name_in, name_out, key):
+        t = [(f[key], int(f[name_in]) + int(f[name_out])) for f in self.flow
+             if name_in in f and name_out in f]
+        u = t.sort(key=lambda x: x[0])
+        u = {key: sum(x[1] for x in group) for key, group in groupby(t, key=lambda x: x[0])}
+        return u
+
+    def __get_flows(self, name, value):
         t = [flow for flow in self.flow
-             if flow.get("protocolName", None) == protocol]
+             if flow.get(name, None) == value]
         return t
+
+    @lru_cache(maxsize=5)
+    def __count(self, name):
+        t = [flow.get(name, None) for flow in self.flow]
+        return Counter(t)
