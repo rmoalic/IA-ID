39c97f37542b0defee166402b111f9d38acb4742
polms <robin.moalic@yahoo.fr>
2019-09-18 17:59:22 +0200

    ES

--- intruvu/__main__.py
+++ intruvu/__main__.py
@@ -1,44 +1,50 @@
 import argparse
 import shelve
 import matplotlib.pyplot as plt
+from elasticsearch import Elasticsearch
 from intruvu.flow import Flow
-from intruvu.loader import load_files
+from intruvu.loader import load_files, index_files
 
 arg_parser = argparse.ArgumentParser(description='Intruder detection')
 arg_parser.add_argument("--cache", type=str, nargs=1, default="fourre-tout", required=False, help="name of the cache file")
 arg_parser.add_argument("--dir", type=str, nargs=1, default="./ISCX_train", required=False, help="directory to load the xml files from")
-arg_parser.add_argument("-e", type=bool, nargs=1, required=False, help="process one file only")
+arg_parser.add_argument("-e", action='store_true', required=False, help="process one file only")
 
 args = arg_parser.parse_args()
 
 FT = args.cache
 
-with shelve.open(FT, 'c') as ft:
-    load_files(args.dir, ft)
+es = Elasticsearch()
+print(es.info())
 
-ft = shelve.open(FT, 'r')
+index_files(args.dir, "flow", es)
 
-if args.e:
-    flow = Flow(ft[list(ft.keys())[1]])
-else:
-    flow = Flow([x for xs in ft.values() for x in xs])
+#with shelve.open(FT, 'c') as ft:
+#    load_files(args.dir, ft)
 
-per = flow.get_flows_per_packet()
-plt.loglog(*zip(*sorted(per.items())), linestyle='None', marker=".")
-plt.xlabel("packet/flow")
-plt.ylabel("flows")
-plt.show()
-
-print(flow.get_protocols())
-print(flow.get_flows_for_protocol("igmp"))
-print(flow.get_flows_count_by_protocol())
-print(flow.get_payload_by_protocol())
-print(flow.get_bytes_count_by_protocol())
-print(flow.get_packets_count_by_protocol())
-print(flow.get_applications())
-print(flow.get_flows_for_application("WebMediaAudio"))
-print(flow.get_payload_by_application())
-print(flow.get_bytes_count_by_application())
-print(flow.get_packets_count_by_application())
-
-ft.close()
+# ft = shelve.open(FT, 'r')
+#
+# if args.e:
+#     flow = Flow(ft[list(ft.keys())[1]])
+# else:
+#     flow = Flow([x for xs in ft.values() for x in xs])
+#
+# per = flow.get_flows_per_packet()
+# plt.loglog(*zip(*sorted(per.items())), linestyle='None', marker=".")
+# plt.xlabel("packet/flow")
+# plt.ylabel("flows")
+# plt.show()
+#
+# print(flow.get_protocols())
+# print(flow.get_flows_for_protocol("igmp"))
+# print(flow.get_flows_count_by_protocol())
+# print(flow.get_payload_by_protocol())
+# print(flow.get_bytes_count_by_protocol())
+# print(flow.get_packets_count_by_protocol())
+# print(flow.get_applications())
+# print(flow.get_flows_for_application("WebMediaAudio"))
+# print(flow.get_payload_by_application())
+# print(flow.get_bytes_count_by_application())
+# print(flow.get_packets_count_by_application())
+#
+# ft.close()
--- intruvu/loader.py
+++ intruvu/loader.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
 from os import listdir, path
-
+import json
 
 def load_flows(file_name):
     """Load a list of flows from an xml file
@@ -40,3 +40,40 @@ def load_files(dir_a, store=None):
         except Exception as e:
             print(e)
     return store
+
+
+def index_files(dir_a, index_name, es):
+    files = [path.join(dir_a, x) for x in listdir(dir_a) if ".xml" in x]
+    if es.indices.exists(index_name):
+        print("dropping index: {}".format(index_name))
+        es.indices.delete(index=index_name)
+    request_body = dict()
+    request_body["settings"] = dict()
+    request_body["settings"]["number_of_shards"] = 1
+    request_body["settings"]["number_of_replicas"] = 0
+    print("creating index: {}".format(index_name))
+    es.indices.create(index=index_name, body=json.dumps(request_body))
+    for f in files:
+        print("loading file {}".format(f))
+        try:
+            flows = load_flows(f)
+            bulk_data = list()
+            for i, flw in enumerate(flows):
+                op_dict = dict()
+                op_dict["index"] = dict()
+                op_dict["index"]["_index"] = index_name
+                op_dict["index"]["_type"] = "tata"
+                op_dict["index"]["_id"] = "{}-{}".format(f, i)
+                bulk_data.append(json.dumps(op_dict))
+                bulk_data.append(json.dumps(flw))
+                if i % 50000 == 0:
+                    print("bulk insert for {}, {}".format(f, i))
+                    es.bulk(index=index_name, body=bulk_data, refresh=True)
+                    bulk_data = list()
+            print("bulk insert for {}: end".format(f))
+            es.bulk(index=index_name, body=bulk_data, refresh=True)
+        except MemoryError:
+            print('Not enough RAM.')
+        except Exception as e:
+            print(e)
+
