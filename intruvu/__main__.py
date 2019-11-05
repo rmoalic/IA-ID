a82347ac95413d4fdb4dba47c39f16df82f87c77
Robin Moalic <robin.moalic@yahoo.fr>
2019-11-05 14:06:30 +0100

    cleanup

--- intruvu/__main__.py
+++ intruvu/__main__.py
@@ -2,12 +2,11 @@ import argparse
 import shelve
 import random
 import sys
-import matplotlib.pyplot as plt
 from elasticsearch import Elasticsearch
 from intruvu.flow import Flow
 from intruvu.flowES import FlowES
 from intruvu.loader import load_files, index_files
-from intruvu.ml import partition, stats
+from intruvu.ml import train_classifier
 
 arg_parser = argparse.ArgumentParser(description='Intruder detection')
 arg_parser.add_argument("--cache", type=str, nargs=1, default="fourre-tout", required=False, help="name of the cache file")
@@ -42,55 +41,14 @@ ft = shelve.open(FT, 'r')
 # plt.ylabel("flows")
 # plt.show()
 
-def make_group(n, part):
-    ret = list()
-    group_size = n // part
-    for i in range(part):
-        ret.extend([i for j in range(group_size)])
-    reste = n % part
-    if reste > 0:
-        ret.extend([0 for i in range(reste)])
-    return np.array(ret)
+vect_l, expected_l = flow.get_vectors_for_application("SMTP")
 
-import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import SGDClassifier
 from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import GroupKFold
-from sklearn.utils import shuffle
-from sklearn.metrics import roc_curve
 
-vect_l, expected_l = flow.get_vectors_for_application("SMTP")
-
-X = np.array(vect_l)
-y = np.array(expected_l)
-
-X, y = shuffle(X, y)
-
-group_kfold = GroupKFold(n_splits=5)
-
-group = make_group(len(X), 5)
-
-def plot_roc_curve(fpr, tpr, name):
-    plt.plot(fpr, tpr, color='orange', label='ROC')
-    plt.plot([0, 1], [0, 1], color='darkblue', linestyle='--')
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('ROC {}'.format(name))
-    plt.legend()
-    plt.show()
-
-for train_index, test_index in group_kfold.split(X, y, group):
-    neigh = MLPClassifier(solver="adam")
-    neigh.fit(X[train_index], y[train_index])
-
-    probs = neigh.predict_proba(X[test_index])[:, 1] # positive class proba
-    fpr, tpr, thresholds = roc_curve(y[test_index], probs)
-    plot_roc_curve(fpr, tpr, type(neigh).__name__)
-
-    stats_r = stats(neigh, X[test_index], y[test_index])
-    print("accuracy {:.4f} time {:4.0f}ms false_positive {:4d} false_negative {:4d} recall {:.4f} prec {:.4f} F1 {:.4f}".format(*stats_r))
+train_classifier(MLPClassifier(), vect_l, expected_l)
 
 
 
--- intruvu/ml.py
+++ intruvu/ml.py
@@ -1,5 +1,10 @@
 import time
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn import metrics
+from sklearn.model_selection import GroupKFold
+from sklearn.utils import shuffle
+from sklearn.metrics import roc_curve
 
 def stats(classifier, X, y):
     assert len(X) == len(y)
@@ -28,6 +33,48 @@ def stats(classifier, X, y):
     REC = metrics.recall_score(y, pred, average='macro')
 
     return accuracy, (end_time - start_time) * 1000, false_positive, false_negative, REC, PREC, F1
+
+
+def train_classifier(classifier, vect_l, expected_l, nb_group=5):
+    X = np.array(vect_l)
+    y = np.array(expected_l)
+
+    X, y = shuffle(X, y)
+
+    group_kfold = GroupKFold(n_splits=5)
+
+    group = make_group(len(X), 5)
+
+    for train_index, test_index in group_kfold.split(X, y, group):
+        classifier.fit(X[train_index], y[train_index])
+
+        probs = classifier.predict_proba(X[test_index])[:, 1]  # positive class proba
+        fpr, tpr, thresholds = roc_curve(y[test_index], probs)
+        plot_roc_curve(fpr, tpr, type(classifier).__name__)
+
+        stats_r = stats(classifier, X[test_index], y[test_index])
+        print("accuracy {:.4f} time {:4.0f}ms false_positive {:4d} false_negative {:4d} recall {:.4f} prec {:.4f} F1 {:.4f}".format(*stats_r))
+
+
+def plot_roc_curve(fpr, tpr, name):
+    plt.plot(fpr, tpr, color='orange', label='ROC')
+    plt.plot([0, 1], [0, 1], color='darkblue', linestyle='--')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC {}'.format(name))
+    plt.legend()
+    plt.show()
+
+
+def make_group(n, part):
+    ret = list()
+    group_size = n // part
+    for i in range(part):
+        ret.extend([i for j in range(group_size)])
+    reste = n % part
+    if reste > 0:
+        ret.extend([0 for i in range(reste)])
+    return np.array(ret)
 
 
 def partition(data, n):
